6bb8554665ae22652ea4569b49824e0ecb5131aa
harish08102004 <116169377+harish08102004@users.noreply.github.com>
2023-03-09 09:38:35 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,193 @@
+var batsman0 = document.getElementById('Dhoni');
+var batsman1 = document.getElementById('Virat');
+var batsman2 = document.getElementById('Sehwag');
+var batsman3 = document.getElementById('Sachin');
+var batsman4 = document.getElementById('Yuvaraj');
+var batsman5 = document.getElementById('Gambir');
+var batsman6 = document.getElementById('Suresh');
+var batsman7 = document.getElementById('Zaheer');
+var batsman8 = document.getElementById('Singh');
+var batsman9 = document.getElementById('Munaf');
+var batsman10 = document.getElementById('Sreesanth');
+
+var batsman11 = document.getElementById('Dhoni1');
+var batsman22 = document.getElementById('Virat1');
+var batsman33 = document.getElementById('Sehwag1');
+var batsman44 = document.getElementById('Sachin1');
+var batsman55 = document.getElementById('Yuvaraj1');
+var batsman66 = document.getElementById('Gambir1');
+var batsman77 = document.getElementById('Suresh1');
+var batsman88 = document.getElementById('Zaheer1');
+var batsman99 = document.getElementById('Singh1');
+var batsman100 = document.getElementById('Munaf1');
+var batsman111 = document.getElementById('Sreesanth1');
+
+batsman11.style.display = 'none';
+
+batsman0.onclick = () => {
+  batsman11.style.display = 'block';
+  batsman33.style.display = 'none';
+  batsman44.style.display = 'none';
+  batsman55.style.display = 'none';
+  batsman66.style.display = 'none';
+  batsman77.style.display = 'none';
+  batsman88.style.display = 'none';
+  batsman99.style.display = 'none';
+  batsman100.style.display = 'none';
+  batsman111.style.display = 'none';
+  batsman22.style.display = 'none';
+};
+
+batsman22.style.display = 'none';
+
+batsman1.onclick = () => {
+  batsman22.style.display = 'block';
+  batsman11.style.display = 'none';
+  batsman33.style.display = 'none';
+  batsman44.style.display = 'none';
+  batsman55.style.display = 'none';
+  batsman66.style.display = 'none';
+  batsman77.style.display = 'none';
+  batsman88.style.display = 'none';
+  batsman99.style.display = 'none';
+  batsman100.style.display = 'none';
+  batsman111.style.display = 'none';
+};
+
+batsman33.style.display = 'none';
+
+batsman2.onclick = () => {
+  batsman33.style.display = 'block';
+  batsman22.style.display = 'none';
+  batsman44.style.display = 'none';
+  batsman55.style.display = 'none';
+  batsman66.style.display = 'none';
+  batsman77.style.display = 'none';
+  batsman88.style.display = 'none';
+  batsman99.style.display = 'none';
+  batsman100.style.display = 'none';
+  batsman111.style.display = 'none';
+  batsman11.style.display = 'none';
+};
+
+batsman44.style.display = 'none';
+
+batsman3.onclick = () => {
+  batsman44.style.display = 'block';
+  batsman33.style.display = 'none';
+  batsman55.style.display = 'none';
+  batsman66.style.display = 'none';
+  batsman77.style.display = 'none';
+  batsman88.style.display = 'none';
+  batsman99.style.display = 'none';
+  batsman100.style.display = 'none';
+  batsman111.style.display = 'none';
+  batsman22.style.display = ' none';
+  batsman11.style.display = 'none';
+};
+batsman55.style.display = 'none';
+
+batsman4.onclick = () => {
+  batsman55.style.display = 'block';
+  batsman44.style.display = 'none';
+  batsman66.style.display = 'none';
+  batsman77.style.display = 'none';
+  batsman88.style.display = 'none';
+  batsman99.style.display = 'none';
+  batsman100.style.display = 'none';
+  batsman111.style.display = 'none';
+  batsman33.style.display = 'none';
+  batsman22.style.display = ' none';
+  batsman11.style.display = 'none';
+};
+batsman66.style.display = 'none';
+
+batsman5.onclick = () => {
+  batsman66.style.display = 'block';
+  batsman44.style.display = 'none';
+  batsman55.style.display = 'none';
+  batsman77.style.display = 'none';
+  batsman88.style.display = 'none';
+  batsman99.style.display = 'none';
+  batsman100.style.display = 'none';
+  batsman111.style.display = 'none';
+  batsman33.style.display = 'none';
+  batsman22.style.display = ' none';
+  batsman11.style.display = 'none';
+};
+batsman77.style.display = 'none';
+
+batsman6.onclick = () => {
+  batsman77.style.display = 'block';
+  batsman44.style.display = 'none';
+  batsman55.style.display = 'none';
+  batsman66.style.display = 'none';
+  batsman88.style.display = 'none';
+  batsman99.style.display = 'none';
+  batsman100.style.display = 'none';
+  batsman111.style.display = 'none';
+  batsman33.style.display = 'none';
+  batsman22.style.display = ' none';
+  batsman11.style.display = 'none';
+};
+batsman88.style.display = 'none';
+
+batsman7.onclick = () => {
+  batsman88.style.display = 'block';
+  batsman44.style.display = 'none';
+  batsman55.style.display = 'none';
+  batsman66.style.display = 'none';
+  batsman77.style.display = 'none';
+  batsman99.style.display = 'none';
+  batsman100.style.display = 'none';
+  batsman111.style.display = 'none';
+  batsman33.style.display = 'none';
+  batsman22.style.display = ' none';
+  batsman11.style.display = 'none';
+};
+batsman99.style.display = 'none';
+
+batsman8.onclick = () => {
+  batsman99.style.display = 'block';
+  batsman44.style.display = 'none';
+  batsman55.style.display = 'none';
+  batsman66.style.display = 'none';
+  batsman77.style.display = 'none';
+  batsman88.style.display = 'none';
+  batsman100.style.display = 'none';
+  batsman111.style.display = 'none';
+  batsman33.style.display = 'none';
+  batsman22.style.display = ' none';
+  batsman11.style.display = 'none';
+};
+batsman100.style.display = 'none';
+
+batsman9.onclick = () => {
+  batsman100.style.display = 'block';
+  batsman44.style.display = 'none';
+  batsman55.style.display = 'none';
+  batsman66.style.display = 'none';
+  batsman77.style.display = 'none';
+  batsman88.style.display = 'none';
+  batsman99.style.display = 'none';
+  batsman111.style.display = 'none';
+  batsman33.style.display = 'none';
+  batsman22.style.display = ' none';
+  batsman11.style.display = 'none';
+};
+
+batsman111.style.display = 'none';
+
+batsman10.onclick = () => {
+  batsman111.style.display = 'block';
+  batsman44.style.display = 'none';
+  batsman55.style.display = 'none';
+  batsman66.style.display = 'none';
+  batsman77.style.display = 'none';
+  batsman88.style.display = 'none';
+  batsman99.style.display = 'none';
+  batsman100.style.display = 'none';
+  batsman33.style.display = 'none';
+  batsman22.style.display = ' none';
+  batsman11.style.display = 'none';
+};
